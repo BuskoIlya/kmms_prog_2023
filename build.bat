2925905af91588d0093b850cb643ee9737b94946
Busko Ilya <trassae95st@mail.ru>
2024-02-11 20:57:48 +0300

    [build.bat] Use 'for' for copy run_xxx.bat scripts

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
-
+setlocal EnableDelayedExpansion
+::
 :: Вид сборки / суффикс в названии каталога сборки
 ::
 :: "Ninja" / "ninja"
@@ -20,5 +21,12 @@ cd %BUILD_FOLDER%
 cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
 cmake --build .
 
-copy ..\%SOURCE_FOLDER%\bubble_sort\run_bubble_sort.bat .\bubble_sort
-copy ..\%SOURCE_FOLDER%\bubble_sort_mf\run_bubble_sort_mf.bat .\bubble_sort_mf
+set arr[0].file=run_bubble_sort.bat
+set arr[1].file=run_bubble_sort_mf.bat
+
+set arr[0].folder=bubble_sort
+set arr[1].folder=bubble_sort_mf
+
+for /L %%i in (0,1,1) do ( 
+	copy ..\%SOURCE_FOLDER%\!arr[%%i].folder!\!arr[%%i].file! .\!arr[%%i].folder!
+)
